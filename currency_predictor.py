9674643639753522f0361ed4d167d6b26e108588
Luis Antonio <luis.antonio.v.r.90@gmail.com>
2018-05-05 20:22:39 -0500

    Get float value from investing USD representation

--- currency_predictor.py
+++ currency_predictor.py
@@ -4,9 +4,14 @@ import pandas as pd
 
 # Save all file matches
 csv_files = glob.glob("*.csv")
+
+# Read csv file
 df = pd.read_csv(csv_files[0])
+new_df = pd.DataFrame()
 
-usd_price = df["Último"]
+new_df["Date"] = df["Fecha"]
+new_df["USD Value"] = df["Último"]
+new_df["Float USD Value"] = df["Último"].apply(lambda x: float(x.replace('.', '').replace(',', '.')))
 
-print(df.head())
-print(usd_price.head())
+print("DataFrame:\n", df.head())
+print("New DF:\n", new_df.head())
