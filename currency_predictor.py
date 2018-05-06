b28e450b4de205343f1f2424696140964994ceeb
Luis Antonio <luis.antonio.v.r.90@gmail.com>
2018-05-05 21:21:01 -0500

    Dolar price from forex-python, adding MXN column to new_df

--- currency_predictor.py
+++ currency_predictor.py
@@ -1,6 +1,10 @@
 # Modules
 import glob
 import pandas as pd
+from forex_python.converter import CurrencyRates
+
+# Rate USD  MXN
+dolar_price = CurrencyRates().get_rate('USD', 'MXN')
 
 # Save all file matches
 csv_files = glob.glob("*.csv")
@@ -12,6 +16,7 @@ new_df = pd.DataFrame()
 new_df["Date"] = df["Fecha"]
 new_df["USD Value"] = df["Último"]
 new_df["Float USD Value"] = df["Último"].apply(lambda x: float(x.replace('.', '').replace(',', '.')))
+new_df["MXN"] = new_df["Float USD Value"].apply(lambda x: x * dolar_price)
 
 print("DataFrame:\n", df.head())
-print("New DF:\n", new_df.head())
+print("\nNew DF:\n", new_df.head())
