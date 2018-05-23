4cdb4e0413e9a4e5d4de456e4e3ee9935286baec
Luis Antonio <luis.antonio.v.r.90@gmail.com>
2018-05-22 22:27:19 -0500

    Change the algorithm

--- currency_predictor.py
+++ currency_predictor.py
@@ -21,21 +21,24 @@ df = pd.read_csv(csv_files[0])
 assert pd.notnull(df).all().all()
 new_df = pd.DataFrame()
 
+# Buil new DataFrame
 new_df["Date"] = pd.to_datetime(df["Fecha"])
 new_df["USD Value"] = df["Último"]
-new_df["Float USD Value"] = df["Último"].apply(lambda x: float(x.replace('.', '').replace(',', '.')))
-new_df["MXN"] = new_df["Float USD Value"].apply(lambda x: x * dollar_price)
+new_df["Float_USD_Value"] = df["Último"].apply(lambda x: float(x.replace('.', '').replace(',', '.')))
 
+date = new_df['Date']
+eth = new_df['Float_USD_Value']
+
+# Set date
 new_df.set_index("Date", inplace=True)
 
 print("DataFrame:\n", df.head())
 print("\nNew DF:\n", new_df.head())
-print("\nSummary statistics:\n", new_df.MXN.describe())
 print("\ndf shape:{}, new_df shape:{}".format(df.shape, new_df.shape))
 
-"""
-new_df.MXN['2018-01-01': '2018-05-05'].plot()
+# Plot
+new_df.Float_USD_Value['2018-01-01': '2018-05-05'].plot(kind='area', alpha=0.4, color='blue')
 plt.title("Ether value")
-plt.ylabel("MXN")
+plt.ylabel("USD")
 plt.show()
-"""
+
